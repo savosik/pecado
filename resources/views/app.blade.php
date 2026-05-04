066de554a44317c7770e44acb4f10863a142a82d
savosik <savosik@savosik-PC>
2026-05-04 10:39:56 +0300

    feat(brand): новый favicon с прозрачным фоном [fast]
    Заменили дефолтный Laravel-favicon на фирменный логотип «О» с крыльями.
    Сгенерированы ico (multi-size) + png 16/32/192/512 + apple-touch-icon 180.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 
         <!-- Fonts: preload наиболее используемых весов -->
         <link rel="preload" href="/fonts/jost/jost-latin-400-normal.woff2" as="font" type="font/woff2" crossorigin>
